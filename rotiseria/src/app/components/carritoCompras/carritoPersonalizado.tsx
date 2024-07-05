93dfdff32e8c80154777afe96e76c07804e94200
Ruben-Torres25 <ruben.010.torres@gmail.com>
2024-07-05 12:08:56 -0300

    inicio de carrito

---
+++ rotiseria/src/app/components/carritoCompras/carritoPersonalizado.tsx
@@ -0,0 +1,15 @@
+export const CarritoPersonalizado = () => {
+   
+    return(
+        
+        <>
+        <div className="contenedorCarrito"></div>
+            <div className="carrito">
+                <div className="presentacion"></div>
+                <h2>Carrito de compras</h2>
+            </div>
+        </>
+
+    )
+} 
+export default CarritoPersonalizado
