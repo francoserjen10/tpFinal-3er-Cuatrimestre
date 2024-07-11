efd3a3647ceb22199f724c6336653dcda03241e0
Franco_Serjen <francoserjen100@gmail.com>
2024-07-11 20:49:39 -0300

    Modificacion de archivo nuevo y su ruteo listo para usar

--- rotiseria/src/app/(routes)/user/carrito/page.tsx
+++ rotiseria/src/app/(routes)/user/carrito/page.tsx
@@ -1,19 +1,14 @@
-"use client";
-import ShoppingCart from "@/app/components/carritoCompras/carritoCompras";
-import CarritoPersonalizado from "@/app/components/carritoCompras/carritoPersonalizado";
-import { Logo } from "@/app/components/logo/logo";
-import { NavBar } from "@/app/components/navHome/navBar";
-import { withRoles } from "@/app/HOC/withRoles";
-
-const MiCarritoUser = () => {
-  return (
-    <>
-      <Logo />
-      <NavBar />
-      <CarritoPersonalizado />
-      <ShoppingCart />
-    </>
-  );
-};
-
-export default withRoles(MiCarritoUser, [2], "/home");
+'use client'
+import { CarritoTabla } from "@/app/components/CarritoTabla/carritoTabla"
+import { Logo } from "@/app/components/logo/logo"
+import NavBar from "@/app/components/navHome/navBar"
+export default function Home() {
+    return(
+        <>
+        <Logo/>
+        <NavBar/>
+        <CarritoTabla/>
+        </>
+    
+    )
+}
